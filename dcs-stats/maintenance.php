bab41f1b2dccd480e77dce3a5702df174a7bc379
Penfold-88 <paul.ludditt@live.co.uk>
2025-08-03 19:11:30 +0100

    Refactor maintenance page

---
+++ dcs-stats/maintenance.php
@@ -0,0 +1,18 @@
+<?php
+http_response_code(503);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>DCS Statistics Dashboard</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="styles-mobile.css">
+</head>
+<body>
+<div class="maintenance-page">
+    <div class="maintenance-icon">✖</div>
+    <p>DCS Statistics Dashboard is unavailable please try again later</p>
+</div>
+</body>
+</html>
