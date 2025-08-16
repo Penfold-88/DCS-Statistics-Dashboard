12ede4a038050c9f933763778eaf8641aebb6e0e
Penfold-88 <paul.ludditt@live.co.uk>
2025-08-16 18:53:12 +0100

    feat: allow configurable site title gradient

--- dcs-stats/header.php
+++ dcs-stats/header.php
@@ -71,6 +71,10 @@ if (isset($_GET['preview']) && $_GET['preview'] === '1') {
         'text_color' => isset($_GET['text']) ? '#' . $_GET['text'] : null,
         'link_color' => isset($_GET['link']) ? '#' . $_GET['link'] : null,
         'border_color' => isset($_GET['border']) ? '#' . $_GET['border'] : null,
+        'title_color' => isset($_GET['title']) ? '#' . $_GET['title'] : null,
+        'title_color_secondary' => isset($_GET['title_secondary']) ? '#' . $_GET['title_secondary'] : null,
+        'table_color' => isset($_GET['table']) ? '#' . $_GET['table'] : null,
+        'table_header_color' => isset($_GET['table_header']) ? '#' . $_GET['table_header'] : null,
     ];
 }
 
